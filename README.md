478d5c5658c08720f12b6a35f7774a3b0f31b0ab
SeonsikYun <42165194+suns2131@users.noreply.github.com>
2022-03-19 23:51:36 +0900

    추가 리액트 업로드
    오늘 공부한 내용 업로드

--- README.md
+++ README.md
@@ -8,6 +8,10 @@
  ### 태그
  - 정의된 요소의 시작하고 끝을 내는 도구로 태그라고 지칭. <br>
    ex) <> , </>
+ 
+ ### 속성 
+ - 요소의 시작태그안에서 사용하는 구체화된 명령어 체계 <br>
+   > \< p **align = "center">** \</p>
  ### DOM(Document Object Model)
  - 문서 객체 모델 <br>
     - 문서 객체모델이란?
@@ -18,9 +22,43 @@
     - 같은 계층에 존재할 경우 형제 관계, 계층이 다르면 부모 자식 관계형태를 가집니다.
 <img src="http://www.tcpschool.com/lectures/img_js_htmldom.png"
 width= "500" height = "300">
- 
+    - DOM 구조 확인법 
+    ```js
+    //현재 html 문서의 DOM 전체 구조 조회 
+    document
+    //DOM 구조 중 body의 내용 확인 (document와 자식관계)
+    document.body
+    //하위 요소에 접근하는 방법1(ChildNodes)
+    //현재 요소의 자식 노드의 전체 Nodelist를 반환한다. 
+    //요소노드 뿐만 아니라 비요소 노드도 포함한 전체를 가져온다.
+    document.body.childNodes
+
+    //하위 요소에 접근하는 방법2(Children)
+    //현재 요소의 자식노드 중 요소 노드만 포함하여 반환한다.
+    document.body.children
+    ```
+ ### 기타 
+  - Document 관련 속성은 Document MDN 참고하면되는데, MDN에서 휴지통 아이콘을 가지고 있는 속성은 현재 사용되지 않는 속성이니 유의할 것. 
 
 ## CSS 정리 
+ ### Selector(선택자)
+  - 스타일을 적용할 대상을 선택하기 위해 선택자를 사용함.
+  - 선택자의 종류 
+    - 전체 : 스타일을 적용할 대상이 HTML 문서 전체로 설정
+    ```js
+    <style>
+      * { background-color : blue}
+    <style>
+    ```
+    - HTML 요소 
+    
+    - ID 
+    - class
+    - Group 
+    - 요소의 특정 상태(수도 클래스)
+
+ ### Gird 시스템 
+ ### CSS 사칙연산 
 
 ## 자바스크립트 정리 
 
---
+++ bucket_list/src/App.js
@@ -0,0 +1,75 @@
+import logo from './logo.svg';
+// import './style.css';
+import React from 'react';
+import styled from "styled-components";
+import BucketLIst from './BucketList';
+
+class App extends React.Component{
+  constructor(props){
+      super(props);
+      this.state = {
+        list : ["영화관 가기", "매일 책 읽기", "맛집 가기"],
+      }
+  }
+
+  render(){
+    console.log(this.state.list)
+    return (
+      <div className='App'>
+        <MyStyled>
+          <div className='container'>
+            <h1>내 버킷 리스트</h1>
+            <hr className='line'/>
+            <BucketLIst list_a = {this.state.list}/>
+          </div>  
+        </MyStyled>
+                
+      </div>
+    );
+  }
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <BucketLIst/>
+//     </div>
+//   );
+// }
+
+const MyStyled = styled.div`
+  background-color: #eee;
+  height: 100vh; /*vh : 화면기준*/
+  width: 100vw;
+  display: flex;
+
+  .container {
+    background-color: #fff;
+    width: 50vw;
+    max-width: 350px;
+    height: 80vh;
+    margin: auto;
+    border-radius: 5px;
+    border: 1px solid #ddd;
+    padding: 16px;
+  }
+  h1 {
+    color:darkblue;
+    text-align: center;
+  }
+  hr{
+    margin: 16px 0px; /*두개로 줄떄는 좌우, 상하 순임. */
+  }
+  .wish_list {
+    padding: 16px;
+    margin: 8px;
+    background-color: #eadd;
+    &:hover{
+      background-color : red;
+    };
+  }
+}
+`;
+/* 스타일 컴포넌트에서 나 자신 지칭 & , 자식요소 스타일에는 안에서 태그명 선언해서 하면됨. */ 
+
+export default App;
